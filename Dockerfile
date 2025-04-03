07a4b37f32668baf1a23a33d977e39b27363b4f6
tillsunrise <71312152+tillsunrise@users.noreply.github.com>
2025-04-03 02:02:31 +0000

    fix mkplayer

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,6 @@ RUN apk add --no-cache python3 youtube-dl \
     && chmod a+rx /usr/local/bin/yt-dlp
 
 COPY ./precompiled/* /app/
-COPY ./src/bootstrap /app/bootstrap
 COPY ./*.crt /app/
 COPY ./*.key /app/
 
--- src/bootstrap/mkplayer.js
+++ src/bootstrap/mkplayer.js
@@ -11,12 +11,29 @@ async function initializeMkPlayer() {
             
             res.on('end', () => {
                 try {
+                    // 创建一个模拟的document对象
+                    global.document = {
+                        createElement: () => ({
+                            src: '',
+                            addEventListener: () => {}
+                        })
+                    };
+                    
                     // 执行获取到的JavaScript代码来初始化mkPlayer
                     eval(data);
+                    
+                    // 清理模拟的document对象
+                    delete global.document;
+                    
                     // 检查mkPlayer是否成功初始化
-                    if (typeof global.mkPlayer === 'undefined') {
+                    if (typeof global.mkPlayer === 'undefined' && typeof mkPlayer !== 'undefined') {
                         global.mkPlayer = mkPlayer;
                     }
+                    
+                    if (typeof global.mkPlayer === 'undefined') {
+                        throw new Error('mkPlayer initialization failed');
+                    }
+                    
                     resolve();
                 } catch (error) {
                     reject(new Error('Failed to initialize mkPlayer: ' + error.message));
